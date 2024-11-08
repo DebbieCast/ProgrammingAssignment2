e425cd09b5abc78a8f8e81c1a6cd41d6f2f0d75c
Deborah Castillo <deborahcastillo95@gmail.com>
2024-11-08 15:57:59 -0600

    Creating the 1st part where we create a funciton that will store the matrix value

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,15 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+            i <- NULL               ## inversion is an empty variable for now
+            set <- function(y) {    ## Setting the value of the matrix
+                        x <<- y
+                        i <<- NULL
+            }
+            get <- function() x     ##Get the value of the matrix
+            setinverse <- function(solve) i <<- solve       ##Set the value of inverse
+            getinverse <- function()   i  ##Get the inverse matrix
+            list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
@@ -13,3 +21,7 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 }
+
+
+
+
