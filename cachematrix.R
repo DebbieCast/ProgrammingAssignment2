72e8f62304d7523e04141cb841a7f1aa703646d2
Deborah Castillo <deborahcastillo95@gmail.com>
2024-11-11 11:29:52 -0600

    Correcting the function. Now it has 2 vectors that will combine to make a matrix and solve its inverse

--- cachematrix.R
+++ cachematrix.R
@@ -1,31 +1,37 @@
 ## 1st part creates the empty matrix that will receive the values
 
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x1 = numeric(), y1 = numeric()) {
             i <- NULL               ## inversion is an empty variable for now
-            set <- function(y) {    ## Setting the value of the matrix
-                        x <<- y
+            set <- function(x2, y2) {    ## Setting the value of the matrix
+                        x1 <<- x2
+                        y1 <<- y2
                         i <<- NULL
             }
-            get <- function() x     ##Get the value of the matrix
-            setinverse <- function(solve) i <<- solve       ##Set the value of inverse
-            getinverse <- function()   i  ##Get the inverse matrix
-            list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
+            if(length(x1)== length(y1)){
+                        z <- rbind(x1,y1)
+                        get <- function()    z ##Get the value of the matrix
+                        setinverse <- function(solve) i <<- solve       ##Set the value of inverse
+                        getinverse <- function()   i  ##Get the inverse matrix
+                        list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)      
+            } else {
+                        message("the matrix must be squared")
+            }
 }
 
 
 ## The 2nd part solves the matrix inverse if its not calculated before
 
-cacheSolve <- function(x, ...) {
-        i <- x$getinverse()
+cacheSolve <- function(z, ...) {
+        i <- z$getinverse()
         if(!is.null(i)) {   ##This makes sure that the matrix inverse was not 
                             ##previously calculated
                     message("getting cached data")
                     return(i)
         }
-        data <- x$get()  ##Retrieving matrix value
+        data <- z$get()  ##Retrieving matrix value
         i <- solve(data, ...)  ##Solving the inverse of the matrix
-        x$setinverse(i)
+        z$setinverse(i)
         i
 }
 
